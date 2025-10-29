dc2bad66cb42ce8fa7f58ab19175793df77e2f81
Luiz <luiz.calmeida@aluno.senac.rio>
2025-10-29 19:36:53 -0300

    teste 2 aula 4

---
+++ UC1/Aula04/aula4v2.py
@@ -0,0 +1,5 @@
+''' Passo a passo para para carregar novo arquivo no github:
+Caso não existam mudanças no repositório da nuvem, execute as mudanças no repositório da local. Ex: Criação de arquivos, exclusão, edição,...
+
+
+'''
