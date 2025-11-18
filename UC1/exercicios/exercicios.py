8cdf60e63337afe3a38c339f064f01fc8dfff393
Luiz <luiz.calmeida@aluno.senac.rio>
2025-11-17 21:46:59 -0300

    aula1 uc2

---
+++ UC1/exercicios/exercicios.py
@@ -0,0 +1,22 @@
+'''def saudacao(nome):
+    print(f"Ola, {nome}! Seja bem vindo!")
+
+usuario=input('Digite seu nome: ')
+saudacao(usuario)'''
+
+'''def dobro(n):
+    return(2*n)
+
+numero=float(input('Digite um número: '))
+print(f'O dobro é {dobro(numero)}')'''
+'''
+def parimpar(n):
+    resto=n%2
+    if resto==0:
+        print('Par')
+    else:
+        print('impar')
+
+numero=float(input('Digite um número:'))
+print=(f'O número é {parimpar(numero)}')'''
+
